52ae279545ff92eab927f3f6a1c14d3940536ac2
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2024-12-01 10:47:47 +0100

    feat(day 1): solution part 1 and 2

---
+++ 2024/day-1/part-2/solution.ts
@@ -0,0 +1,16 @@
+import { readFile } from "node:fs/promises";
+
+const data = await readFile("./puzzel-input.txt", "utf8");
+
+const lines = data
+	.trim()
+	.split("\n")
+	.map((line) => line.split("  "));
+
+const b = lines.map(([_, b]) => b).sort((a, b) => (a < b ? -1 : 1));
+const result = lines
+	.map(([a, _]) => a)
+	.sort((a, b) => (a < b ? -1 : 1))
+	.reduce((sum, c) => sum + +c * b.filter((value) => +value === +c).length, 0);
+
+console.log("result", result);
