3575a2fd8f902af9607e1d0e5246920eeb259dca
Jeroen Zeelmaekers <mail@jeroenzeelmaekers.com>
2024-12-01 13:55:46 +0100

    refactor(day 1): cleanup

--- src/2024/day-01/part-2.ts
+++ src/2024/day-01/part-2.ts
@@ -4,9 +4,9 @@ export default function (blob: string) {
     .split('\n')
     .map((line) => line.trim().split('  '))
 
-  const b = input.map(([_, b]) => b).sort((a, b) => (a < b ? -1 : 1))
+  const b = input.map(([_, b]) => b).sort((a, b) => +a - +b)
   return input
-    .map(([a, _]) => a)
-    .sort((a, b) => (a < b ? -1 : 1))
+    .map(([a, _]) => +a)
+    .sort((a, b) => a - b)
     .reduce((sum, c) => sum + +c * b.filter((value) => +value === +c).length, 0)
 }
